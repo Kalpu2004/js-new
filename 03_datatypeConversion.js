5f1fd81b9d25a23c142c53f6b100950f7ec766d6
Kalpu2004 <140461850+Kalpu2004@users.noreply.github.com>
2024-03-28 19:37:40 +0530

    Add files via upload

---
+++ 03_datatypeConversion.js
@@ -0,0 +1,65 @@
+// let score = "kalpana"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 2; false => 0
+// "kalpana" => NaN 
+
+let isLoggedIn = "Kalpana"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Kalpana" => true
+
+let someNo = 11
+
+let stringNo =String(someNo)
+// console.log(stringNo);
+// console.log(typeof stringNo);
+
+// ******************************** Operations ***************************//
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " dear"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+// console.log(true); =>true
+// console.log(""); =>
+// console.log(+true); =>1
+// console.log(+""); =>0
+
+let num1, num2, num3 
+num1=num2=num3=2+2
+// console.log(num2);
+
+let gameCounter =100
+// gameCounter++;    **prefix and postfix
+// console.log(gameCounter);
+
