4fad6d4a9daafa7feac14d1622e0255489d0af6a
Kalpu2004 <140461850+Kalpu2004@users.noreply.github.com>
2024-03-28 19:38:05 +0530

    Add files via upload

---
+++ 04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0); =>false ...When comparing null with a number using the > operator, JavaScript treats null as 0.
+// console.log(null==0); =>false ...In this case, null is not equal to 0 because null is a primitive value that represents the intentional absence of any object value.
+// console.log(null>=0); =>true ...The >= operator behaves similarly to >, but it returns true when the values are equal. Since null is treated as 0, the comparison 0 >= 0 evaluates to true.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+// console.log("2"===2);
