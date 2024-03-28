e9b99ece319d5c5d4b5018288fbad657d9193659
Kalpu2004 <140461850+Kalpu2004@users.noreply.github.com>
2024-03-28 19:37:15 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 2+3 ) //we are using node.js not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("Kalpana")
